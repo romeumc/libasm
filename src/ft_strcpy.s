92eccea319c6bb399b2cfe4aceefff714084ba72
rmartins <rmartins@student.42lisboa.com>
2021-04-26 17:16:19 +0100

    ft_strcmp

--- src/ft_strcpy.s
+++ src/ft_strcpy.s
@@ -4,17 +4,19 @@ ft_strcpy:
 	push	rbp			; Function Prologue
 	mov		rbp, rsp
 
-	mov		rcx, 0
+	mov		rcx, 0		; rcx = 0
 	
 loop:
-	cmp BYTE[rdi + rcx], 0
-	je exit
-	inc rcx
+	mov		al, [rsi + rcx]
+	mov		[rdi + rcx], al
+	cmp		al, 0
+	je exit					; if (str[i] == '\0') {exit}
+	inc rcx					; rcx++
 	jmp loop
 
 exit:
 	mov		rsp, rbp	; Function Epilogue
 	pop		rbp
 
-	mov		rax, rcx
+	mov		rax, rdi
 	ret
--- src/ft_strlen.s
+++ src/ft_strlen.s
@@ -4,13 +4,13 @@ ft_strlen:
 	push	rbp			; Function Prologue
 	mov		rbp, rsp
 
-	mov		rcx, 0
+	mov		rcx, 0		; rcx = 0
 	
 loop:
-	cmp BYTE[rdi + rcx], 0
-	je exit
-	inc rcx
-	jmp loop
+	cmp		BYTE[rdi + rcx], 0
+	je		exit				; if (str[i] == '\0') {exit}
+	inc		rcx					; rcx++
+	jmp		loop
 
 exit:
 	mov		rsp, rbp	; Function Epilogue
